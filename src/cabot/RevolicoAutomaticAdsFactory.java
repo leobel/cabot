f9021e6a5ab166e18b9652ecf334d7f5103766e4
leobel <leobelizquierdo@gmail.com>
2016-05-24 14:15:37 -0400

    using a pre established order to insert the advertisement fields (this fix the three photos issue).

--- src/cabot/RevolicoAutomaticAdsFactory.java
+++ src/cabot/RevolicoAutomaticAdsFactory.java
@@ -30,9 +30,6 @@ public class RevolicoAutomaticAdsFactory {
     private Map<String, String> scrappOptions;
     private Map<String, String> captchaOptions;
     private CaptchaService service;
-    private Map<String, String> options;
-    private HttpConnection connRes;
-    private HttpConnection connIn;
     private ConnectionConfig config;
     private AutomaticAdsAdapter adapter;
     
@@ -43,21 +40,24 @@ public class RevolicoAutomaticAdsFactory {
     public final static String PICTURE_A = "ad_picture_a";
     public final static String PICTURE_B = "ad_picture_b";
     public final static String PICTURE_C = "ad_picture_c";
+    public final static String FILE_SIZE_NAME = "MAX_FILE_SIZE";
+    public final static String FILE_SIZE = "307200";
     public final static String EMAIL = "email";
     public final static String NAME = "name";
     public final static String PHONE = "phone";
     public final static String EMAIL_ENABLED = "email_enabled";
+    public final static String RECAPTCHA_CHALLENGE_FIELD = "recaptcha_challenge_field";
+    public final static String RECAPTCHA_RESPONSE_FIELD = "recaptcha_response_field";
+    public final static String SEND_FORM = "send_form";
+    public final static String SEND_ACTION = "Enviar";
     private String appDirectory;
     private final RevolicoAdvertisementDao advertisementManager;
     
-    public RevolicoAutomaticAdsFactory(RevolicoAdvertisementDao advertisementManager, String appDirectory, ConnectionConfig config, HttpConnection connIn, HttpConnection connRes, Map<String, String> options, CaptchaService service, 
+    public RevolicoAutomaticAdsFactory(RevolicoAdvertisementDao advertisementManager, String appDirectory, ConnectionConfig config, CaptchaService service, 
             Map<String, String> captchaOptions, Map<String, String> scrappOptions, String host, AutomaticAdsAdapter adapter){
         this.advertisementManager = advertisementManager;
         this.appDirectory = appDirectory;
         this.config = config;
-        this.connIn = connIn;
-        this.connRes = connRes;
-        this.options = options;
         this.service = service;
         this.captchaOptions = captchaOptions;
         this.scrappOptions = scrappOptions;
@@ -90,5 +90,13 @@ public class RevolicoAutomaticAdsFactory {
         ((DesiredCapabilities) caps).setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, appDirectory + File.separator + phantomjs);
         WebDriver driver= new PhantomJSDriver(caps);
         return new RevolicoScrapper(host, driver, service, config, scrapConfig, captchaOptions);
+    }
+    
+    public void setService(CaptchaService service){
+        this.service = service;
+    }
+    
+    public void setScrappOptions(Map<String, String>scrappOptions){
+        this.scrappOptions = scrappOptions;
     }
 }
